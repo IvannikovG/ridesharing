07134de308b1bcee46d5cdba02ea0ab218226cdc
georgii <georgii>
2020-07-13 14:24:11 +0200

    F

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgres://rnforvzzltqhju:3db84efc383bef98cb6933c27e9a77006f330292451916d8adac0a43d3379ae6@ec2-34-233-226-84.compute-1.amazonaws.com:5432/d6soqquhdtkgd0'
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RIDES_PER_PAGE = 5
